de486ca715aac38c199541cf4f63ced99e1db114
Philip Daian <phil@linux.com>
2021-07-30 16:33:35 -0400

    working exec + simulation

--- transactions.py
+++ transactions.py
@@ -6,34 +6,51 @@ class Transaction:
 
 
 class UniswapTransaction(Transaction):
-    def __init__(self, first_token, second_token, first_amount, second_amount, sender, fee):
+    def __init__(self, x_token, y_token, x_amount, y_amount, sender, fee):
         super().__init__(sender, fee)
         # tokens involved in transaction
-        self.first_token = first_token
-        self.second_token = second_token
+        self.x_token = x_token
+        self.y_token = y_token
         # amounts involved in transaction
-        self.first_amount = first_amount
-        self.second_amount = second_amount
+        self.x_amount = x_amount
+        self.y_amount = y_amount
 
 
 class ETHState:
     def __init__(self):
-        self.first_token_amount = 0
-        self.second_token_amount = 0
+        self.x = 0
+        self.y = 0
+
 
 class SwapTransaction(UniswapTransaction):
 
-    def execute(ETHState):
-        pass
+    def _swapquote(self,A, A_reserves, B_reserves): # Computes how much is swaped given the reserves
+        k = A_reserves * B_reserves
+        return B_reserves - k / (A_reserves + A)
 
+    def execute(self, state):
+        if (self.x_amount + self.y_amount == 0):
+            pass
+        elif self.x_amount == 0:
+            # swap for x token
+            amountX = self._swapquote(self.y_amount,state.y, state.x)
+            state.x -= amountX
+            state.y += self.y_amount
+        elif self.y_amount == 0:
+            # swap for y token
+            amountY = self._swapquote(self.x_amount,state.x, state.y)
+            state.y -= amountY
+            state.x += self.x_amount
 
 class AddLiquidityTransaction(UniswapTransaction):
 
-    def execute(ETHState):
-        pass
+    def execute(self, state):
+        state.x += self.x_amount
+        state.y += self.y_amount
 
 class RemoveLiquidityTransaction(UniswapTransaction):
 
-    def execute(ETHState):
+    def execute(self, state):
+        #state.x -= self.x_amount
+        #state.y -= self.y_amount
         pass
-
